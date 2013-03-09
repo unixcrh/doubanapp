9a24e0299be0c2789c90bb13642ddec909cc31a1
mcxiaoke <mcxiaoke@gmail.com>
2013-03-09 23:58:49 +0800

    init doubanapp project at 2013.03.09

---
+++ app/gen/org/mcxiaoke/douban/app/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package org.mcxiaoke.douban.app;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ library/gen/org/mcxiaoke/douban/lib/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package org.mcxiaoke.douban.lib;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
