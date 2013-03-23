42259887e56140a7b96cefae0cbab7a0f7c2caa7
mcxiaoke <mcxiaoke@gmail.com>
2013-03-23 09:55:56 +0800

    remove accountoperations

--- library/src/org/mcxiaoke/douban/DoubanClientFactory.java
+++ library/src/org/mcxiaoke/douban/DoubanClientFactory.java
@@ -3,14 +3,17 @@
  */
 package org.mcxiaoke.douban;
 
+import android.content.Context;
+
 /**
  * @author mcxiaoke
  * 
  */
 public final class DoubanClientFactory {
 
-	public static DoubanClient createDoubanClient() {
-		return null;
+	public static DoubanClient createDoubanClient(Context context,
+			DoubanSession session) {
+		return new DoubanClientImpl(context, session);
 	}
 
 }
